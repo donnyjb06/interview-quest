65f236004bd985a67cb4ae91870f7fe9279383e0
Donovan Jabbar <djabbar51@gmail.com>
2026-05-06 02:30:21 -0500

    fix: have extractProblemSlug accept path name as argument

--- apps/extension/src/content/problem/extractProblemSlug.ts
+++ apps/extension/src/content/problem/extractProblemSlug.ts
@@ -1,8 +1,8 @@
-export const extractProblemSlug = () => {
-  const parts = window.location.pathname.split("/");
+export const extractProblemSlug = (pathName: string): string | null => {
+  const parts = pathName.split("/");
   const index = parts.indexOf("problems");
 
   if (index === -1) return null;
 
-  return parts[index + 1] ?? null;
+  return parts[index + 1] || null;
 };
