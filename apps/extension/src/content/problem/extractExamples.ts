1d95159614ebed87feec57ab05a91439880404d4
Donovan Jabbar <djabbar51@gmail.com>
2026-05-06 02:31:33 -0500

    fix(content): return null if examples array is empty

--- apps/extension/src/content/problem/extractExamples.ts
+++ apps/extension/src/content/problem/extractExamples.ts
@@ -1,10 +1,15 @@
 import type { Example } from "../../lib/types";
 import { extractExample } from "./extractExample";
 
-export const extractExamples = (container: Element): Example[] => {
+export const extractExamples = (container: Element): Example[] | null => {
   const exampleElements: HTMLPreElement[] = Array.from(
     container.querySelectorAll("pre"),
   );
 
-  return exampleElements.map(extractExample);
+  const examples = exampleElements
+    .map(extractExample)
+    .filter((example) => !!example);
+  if (exampleElements.length < 1) return null;
+
+  return examples;
 };
