991061d5a45261a4ae97c2a7bdb600dbbcfe88db
Donovan Jabbar <djabbar51@gmail.com>
2026-05-12 13:56:25 -0500

    fix(build): create path alias for shared package

--- apps/extension/vitest.config.ts
+++ apps/extension/vitest.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "vitest/config";
+import path from "node:path";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      shared: path.resolve(__dirname, "../../packages/shared/src"),
+    },
+  },
   test: {
     environment: "jsdom",
   },
